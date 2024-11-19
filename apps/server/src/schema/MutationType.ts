89184d749465bab761c87aa5c229ee8c8d7a1544
Darlan Jr <darlan.junior@stone.com.br>
2024-11-19 00:05:03 -0400

    fix: update imports

--- apps/server/src/schema/MutationType.ts
+++ apps/server/src/schema/MutationType.ts
@@ -1,7 +1,6 @@
 import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
-
 import { messageMutations } from '../modules/message/mutations/messageMutations';
-import { userMutations } from '../modules/user/mutations/userMutations';
+import { userMutations } from '../modules/mutations/user/userMutations';
 
 export const MutationType = new GraphQLObjectType({
 	name: 'Mutation',
